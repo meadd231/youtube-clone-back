32ea4b8648de75f07fb71923c50d55ca9a30352a
DongHwna Lee <meadding231@gmail.com>
2023-10-28 08:45:22 +0900

    [fix] cors 문제 해결

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 require('dotenv').config();
 const port = process.env.HOST_PORT;
 
@@ -16,12 +17,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // cors
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://192.168.219.104:3000']
+}));
+
 
 // routes
 const routes = require('./routes');
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,61 +5,11 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const authRouter = require('./auth.routes');
 
+// test 용 api
 router.post("/test", (req, res, next) => {
   console.log('req.body', req.body);
   res.send("test success");
 });
-
-router.post("/signup", async (req, res, next) => {
-  console.log(req.body);
-  
-  // active record pattern. 즉, user는 User 테이블의 하나의 레코드가 될 수 있는 객체이다.
-  const user = await User.create(req.body);
-
-  // 비밀번호 해시화
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
-
-  // db에 만든 레코드 저장
-  await user.save();
-
-  // jwt 토큰에 userId 담고 토큰 생성
-  const payload = { id: user.id };
-  const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.ACCESS_EXPIRES,
-  });
-
-  // 응답
-  res.status(200).json({ success: true, data: token });
-});
-
-router.post("/login", async (req, res, next) => {
-  console.log(req.body);
-  const { email, password } = req.body;
-
-  const user = await User.findOne({ where: { email } });
-
-  if (!user) {
-    return next({
-      message: "The email is not yet registered",
-      statusCode: 400,
-    });
-  }
-
-  const passwordMatch = await bcrypt.compare(password, user.password);
-
-  if (!passwordMatch) {
-    return next({ message: "The password does not match", statusCode: 400 });
-  }
-
-  const payload = { id: user.id, nickname: user.nickname };
-  const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.ACCESS_EXPIRES,
-  });
-
-  res.status(200).json({ success: true, token });
-});
-
 router.get("/json-array", (req, res, next) => {
   const array = [];
   for (let i = 0; i < 10; i++) {
