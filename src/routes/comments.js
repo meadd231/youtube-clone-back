c2de5fb17a57d777d119761c38c658aaba5d9d6c
DongHwna Lee <meadding231@gmail.com>
2024-01-05 10:53:06 +0900

    feat: express-validator post comment 시험 적용

--- src/routes/comments.js
+++ src/routes/comments.js
@@ -1,25 +1,31 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
-const { auth } = require("../middleware/auth");
+const { auth, authOption, authNeed } = require("../middleware/auth");
 
 const CommentsController = require("../controllers/comments.controller");
 const commentsController = new CommentsController();
 
-router.post("/comment", auth, commentsController.postComment);
+router.post(
+  "/comment",
+  authNeed,
+  body("content").isLength({ min: 1, max: 100 }),
+  commentsController.postComment
+);
 
-router.post("/replies/reply", auth, commentsController.postReply);
+router.post("/replies/reply", authNeed, commentsController.postReply);
 
 router.get("/count/:videoId", commentsController.getCommentsCount);
 
-router.get("/:videoId", auth, commentsController.getComments);
+router.get("/:videoId", authOption, commentsController.getComments);
 
-router.get("/:commentId/replies", auth, commentsController.getReplies);
+router.get("/:commentId/replies", authOption, commentsController.getReplies);
 
-router.post("/:commentId/like", auth, commentsController.postCommentLike);
+router.post("/:commentId/like", authNeed, commentsController.postCommentLike);
 
 router
   .route("/:commentId")
   .patch(auth, commentsController.patchComment)
-  .delete(auth, commentsController.deleteComment)
+  .delete(auth, commentsController.deleteComment);
 module.exports = router;
